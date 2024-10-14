c657231aa7b04ee0a88ced24bafa4cc1ff0914ed
Anuhya Talamanchi <anuhya92@gmail.com>
2024-10-14 23:11:29 +0200

    final commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//string
+console.log("Hello, World!");
+
+//variables
+let firstName = "Anuhya";
+let age = "32";
+let favouriteColor = "Red";
+console.log(
+  `My name is ${firstName}, I'm ${age} years old and my favourite color is ${favouriteColor}`
+);
+console.log(`Type of firstName ${typeof firstName}`);
+
+//arithmetic operations
+let x = 7;
+let y = 5;
+console.log(`Sum: ${x + y}`);
+console.log(`Subtraction: ${x - y}`);
+console.log(`Product: ${x * y}`);
+console.log(`Division: ${x / y}`);
